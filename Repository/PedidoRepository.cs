dec346c3ca4b6f49a694c585c6324b1978ccb901
Yandy Sanchez <skullcandy961205@gmail.com>
2023-04-16 17:07:20 -0400

    update pedidos

--- Repository/PedidoRepository.cs
+++ Repository/PedidoRepository.cs
@@ -2,11 +2,6 @@
 using Entities;
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repository
 {
