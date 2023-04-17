bb7b48fd6a6392c7c71497a9bc609df9d61c19c3
Yandy Sanchez <skullcandy961205@gmail.com>
2023-04-16 20:29:33 -0400

    Add Responses

---
+++ Project315/BasicResponses/ApiResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Project315.BasicResponses
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = "", int customStatusCode = 0)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            CustomStatusCode = customStatusCode;
+        }
+
+        public int StatusCode { get; }
+        public int CustomStatusCode { get; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Message { get; }
+    }
+}
